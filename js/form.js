b74dd3f761fd3be0e18b5159e367caed7b95fb96
FrosT2k5 <androygaming@gmail.com>
2022-08-10 19:36:42 +0530

    updating contact url switched hosting

--- js/form.js
+++ js/form.js
@@ -4,7 +4,7 @@ function sub() {
 	var email = frm['email'].value
 	var msg = frm['message'].value
 	var req = "Name: " + fullname + "%0A%0AEmail: " + email + "%0A%0AMessage: " + msg
-	var url = "https://frost2k5.000webhostapp.com/contact.php?"+req
+	var url = "http://frost.alwaysdata.net/contact.php?"+req
 	fetch(url)
 	alert("Message sent!")
 }
