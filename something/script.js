e9c35c71a415c762092429859e2a18b762f896cf
FrosT2k5 <androygaming@gmail.com>
2024-01-16 07:49:19 +0530

    Introduce something/ Something's sus hehehe
    Signed-off-by: FrosT2k5 <androygaming@gmail.com>

---
+++ something/script.js
@@ -0,0 +1,45 @@
+function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+async function simulateTyping(elementID, editText, timeout) {
+
+        const domElement = document.getElementById(elementID)
+        let textString = ""
+        const ignoreUpdate = ['<','>']
+
+        for (i=0; i<editText.length; i++) {
+                textString += editText[i]
+                if (!ignoreUpdate.includes(editText[i])) {
+                        domElement.innerHTML = textString + "|"
+                }
+                await sleep(timeout)
+        }
+
+        domElement.innerHTML = textString
+	await sleep(600)
+
+	for (i=textString.length; i>=0; i--) {
+		textString = textString.slice(0,i)
+		if (!ignoreUpdate.includes(editText[i])) {
+                        domElement.innerHTML = textString + "|"
+                }
+		await sleep(45)
+	}
+
+	domElement.innerHTML = textString
+        await sleep(200)
+}
+
+function addFirework() {
+	let fireworkDiv = document.createElement("div")
+	fireworkDiv.setAttribute("class","firework")
+
+	mainDiv = document.getElementById("fireworkContainer");
+        mainDiv.append(fireworkDiv)
+}
+
+function removeFirework() {
+	let fireworkDiv = document.querySelector(".firework")
+	fireworkDiv.remove()
+}
